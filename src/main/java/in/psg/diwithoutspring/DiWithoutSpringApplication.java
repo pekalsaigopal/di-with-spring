bcb1bfd274eb6f9d4839cdc69092ce88457bf325
saigopal <saigopalpekal@gmail.com>
2020-04-30 14:19:56 +0530

    Added DI using Setter Injection

--- src/main/java/in/psg/diwithoutspring/DiWithoutSpringApplication.java
+++ src/main/java/in/psg/diwithoutspring/DiWithoutSpringApplication.java
@@ -1,6 +1,7 @@
 package in.psg.diwithoutspring;
 
 import in.psg.diwithoutspring.controllers.PropertyInjectedController;
+import in.psg.diwithoutspring.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,9 +11,16 @@ public class DiWithoutSpringApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiWithoutSpringApplication.class, args);
+
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		String greetingFromProperty=propertyInjectedController.getGreeting();
 		System.out.println(greetingFromProperty);
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		String greetingFromSetter=setterInjectedController.getGreeting();
+		System.out.println(greetingFromSetter);
+
+
 
 
 	}
---
+++ src/main/java/in/psg/diwithoutspring/controllers/SetterInjectedController.java
@@ -0,0 +1,19 @@
+package in.psg.diwithoutspring.controllers;
+
+import in.psg.diwithoutspring.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+   private GreetingService greetingService;
+
+   @Autowired
+   public void setGreetingService(GreetingService greetingService) {
+      this.greetingService = greetingService;
+   }
+
+   public String getGreeting(){
+      return greetingService.sayGreeting();
+   }
+}

11b49e7e6e4adbec9806898975500c4a9ae35920
saigopal <saigopalpekal@gmail.com>
2020-04-30 18:20:15 +0530

    Default Profile Set

--- src/main/java/in/psg/diwithspring/services/I18nHindiGreetingService.java
+++ src/main/java/in/psg/diwithspring/services/I18nHindiGreetingService.java
@@ -3,7 +3,7 @@ package in.psg.diwithspring.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("HI")
+@Profile({"HI","default"})
 @Service("i18nService")
 public class I18nHindiGreetingService implements GreetingService{
    @Override

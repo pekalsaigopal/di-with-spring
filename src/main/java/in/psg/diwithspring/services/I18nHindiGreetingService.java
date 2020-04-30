9770cbbce2d05f90b4ca6537352f53045eec47d1
saigopal <saigopalpekal@gmail.com>
2020-04-30 17:40:19 +0530

    Refactor package name and Application name

--- src/main/java/in/psg/diwithspring/services/I18nHindiGreetingService.java
+++ src/main/java/in/psg/diwithspring/services/I18nHindiGreetingService.java
@@ -1,14 +1,13 @@
-package in.psg.diwithoutspring.services;
+package in.psg.diwithspring.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile("HI")
 @Service("i18nService")
-public class I18nEnglishGreetingService implements GreetingService{
-
+public class I18nHindiGreetingService implements GreetingService{
    @Override
    public String sayGreeting() {
-      return "Hello From - EN";
+      return "Namaskar - HI";
    }
 }
